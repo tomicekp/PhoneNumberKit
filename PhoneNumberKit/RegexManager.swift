6a61a2ecd4bc81bbe50425dc1663aa0789ce12cc
tomicekp <50492132+tomicekp@users.noreply.github.com>
2019-05-10 16:43:27 +0200

    Update RegexManager.swift

--- PhoneNumberKit/RegexManager.swift
+++ PhoneNumberKit/RegexManager.swift
@@ -13,31 +13,34 @@ final class RegexManager {
     // MARK: Regular expression pool
 
     var regularExpresionPool = [String: NSRegularExpression]()
-
+    let regularExpressionPoolQueue = DispatchQueue(label: "com.phonenumberkit.regexpool", attributes: .concurrent)
     var spaceCharacterSet: CharacterSet = {
         let characterSet = NSMutableCharacterSet(charactersIn: "\u{00a0}")
         characterSet.formUnion(with: CharacterSet.whitespacesAndNewlines)
         return characterSet as CharacterSet
     }()
 
-    deinit {
-        regularExpresionPool.removeAll()
-    }
-
     // MARK: Regular expression
-
+    
     func regexWithPattern(_ pattern: String) throws -> NSRegularExpression {
-        if let regex = regularExpresionPool[pattern] {
-            return regex
-        } else {
-            do {
-                let regularExpression: NSRegularExpression
-                regularExpression =  try NSRegularExpression(pattern: pattern, options:NSRegularExpression.Options.caseInsensitive)
-                regularExpresionPool[pattern] = regularExpression
-                return regularExpression
-            } catch {
-                throw PhoneNumberError.generalError
+        var cached: NSRegularExpression?
+        regularExpressionPoolQueue.sync {
+            cached = self.regularExpresionPool[pattern]
+        }
+        
+        if let cached = cached {
+            return cached
+        }
+        
+        do {
+            let regex = try NSRegularExpression(pattern: pattern, options: .caseInsensitive)
+            regularExpressionPoolQueue.async(flags: .barrier) {
+                self.regularExpresionPool[pattern] = regex
             }
+            return regex
+        }
+        catch {
+            throw PhoneNumberError.generalError
         }
     }
 
